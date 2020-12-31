2670774f0d252abf9a368241b8217845db143722
matsudai <matsudai@users.noreply.github.com>
2020-12-31 12:32:03 +0000

    deploy: ec9818e85927e362b9820dd1412ed171753744b6

---
+++ _next/static/OGnTsqgYCgJpMr5drCLGy/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fcss\u002F17efa99b8ef4dcb6cb9f.css","static\u002Fchunks\u002Fpages\u002Findex-a8975a85602f89d15336.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-85785763783c1e33a9f3.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
