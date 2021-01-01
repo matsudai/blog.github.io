6f4590253b3255f0ea920562e7801dd5b9a98534
matsudai <matsudai.social.1131@gmail.com>
2021-01-01 11:54:32 +0900

    Update ESLint rules for TypeScript import
    * ts/tsx is allowed to import
    * avoid ERROR when import react or define interface in ts/tsx file

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,6 +20,32 @@ module.exports = {
         "react",
         "@typescript-eslint"
     ],
+    "settings": {
+        // TypeScriptのファイルをimportできるようにする。
+        "import/resolver": {
+            "node": {
+                "extensions": [".ts", ".tsx", ".js", ".jsx"]
+            }
+        }
+    },
     "rules": {
+        // TypeScriptのファイルをimportできるようにする。
+        "import/extensions": [2, { ".ts": "never", ".tsx": "never", ".js": "never", ".jsx": "never" }],
+
+        // TypeScriptのファイルでJSXを書けるようにする。
+        "react/jsx-filename-extension": [2, { "extensions": [".ts", ".tsx", ".js", ".jsx"]  }],
+
+        /*
+        * ESLintのルールをTypeScript ESLintのルールで代用するパターン。
+        */
+
+        // importがエラーになることの回避。
+        // 例えば "import React, { FC } from 'react'" したときの "'React' was used before it was defined"。
+        "no-use-before-define": "off",
+        "@typescript-eslint/no-use-before-define": ["error"],
+
+        // 型やinterfaceが解決できないことの回避。
+        "no-unused-vars": "off",
+        "@typescript-eslint/no-unused-vars": "error"
     }
 }
