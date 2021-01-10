c2cad1e59bcf591432fe3499082ee15a4ea0a50d
matsudai <matsudai.social.1131@gmail.com>
2021-01-10 23:45:27 +0900

    Update ESLint to disallow comma-dangle

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,40 +2,43 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
-    node: true,
+    node: true
   },
   extends: [
     'plugin:react/recommended',
-    'airbnb',
+    'airbnb'
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
-      jsx: true,
+      jsx: true
     },
     ecmaVersion: 12,
-    sourceType: 'module',
+    sourceType: 'module'
   },
   plugins: [
     'react',
-    '@typescript-eslint',
+    '@typescript-eslint'
   ],
   settings: {
     // TypeScriptのファイルをimportできるようにする。
     'import/resolver': {
       node: {
-        extensions: ['.ts', '.tsx', '.js', '.jsx'],
-      },
-    },
+        extensions: ['.ts', '.tsx', '.js', '.jsx']
+      }
+    }
   },
   rules: {
     // TypeScriptのファイルをimportできるようにする。
     'import/extensions': [2, {
-      '.ts': 'never', '.tsx': 'never', '.js': 'never', '.jsx': 'never',
+      '.ts': 'never', '.tsx': 'never', '.js': 'never', '.jsx': 'never'
     }],
 
     // TypeScriptのファイルでJSXを書けるようにする。
     'react/jsx-filename-extension': [2, { extensions: ['.ts', '.tsx', '.js', '.jsx'] }],
+
+    // 末尾の要素のカンマをつけない（ESLintのデフォルト）。
+    'comma-dangle': ['error', 'never'],
 
     /*
     * ESLintのルールをTypeScript ESLintのルールで代用するパターン。
@@ -49,5 +52,16 @@ module.exports = {
     // 型やinterfaceが解決できないことの回避。
     'no-unused-vars': 'off',
     '@typescript-eslint/no-unused-vars': 'error',
-  },
+
+    /*
+    * Next.js用の設定。
+    */
+
+    // next/linkを利用した場合の、子要素のanchorにhrefプロパティを設定しないことによるエラーの回避。
+    'jsx-a11y/anchor-is-valid': ['error', {
+      components: ['Link'],
+      specialLink: ['hrefLeft', 'hrefRight'],
+      aspects: [/* 'noHref', */'invalidHref', 'preferButton']
+    }]
+  }
 };
